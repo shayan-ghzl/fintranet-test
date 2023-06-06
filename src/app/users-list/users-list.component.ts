43aafa0d1bf3fb5bc2ebff081a27ecc91bf87c3e
shayan ghazali <shayan97.tti@gmail.com>
2023-06-06 23:25:30 +0430

    github security alert fixed

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -17,15 +17,7 @@ export class UsersListComponent {
 
   @ViewChild('tableSearchFilter') set tableSearchFilter(value: ElementRef<HTMLInputElement>){
     if(value){
-      // why we use it here instead of afterViewInit:
-      // because this.userList$ initial value is null and so search input will not be rendered
-      // so at first it is undefined and we want to check if at next change detection cycle it 
-      // changed 
-      // but if it was in the afterViewInit it would be called once during the cd cycles
       value.nativeElement.focus();
-      // setTimeout(() => {
-      //   value.nativeElement.focus();
-      // }, 0);
     }
   }
 
